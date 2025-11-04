6526bdc469742ee6398d961aa5802e8c527d8065
yhamedish <95268812+yhamedish@users.noreply.github.com>
2025-11-03 21:55:18 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ✅ your repo name
+  base: '/tickwise_frontend/', // ✅ your repo name
 })
