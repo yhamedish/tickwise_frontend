7373e92f96bdfb7bd07f722f3e652615794093d7
yhamedish <yousef.hamedish@gmail.com>
2025-11-03 22:30:30 -0700

    with custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tickwise_frontend/', // ✅ your repo name
+  base: '/', // ✅ your repo name
 })
